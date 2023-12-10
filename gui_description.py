483f45f7aec883e14ca324adbcd8b1d2716d5810
alinkazhoptic <borisova_alina_95@mail.ru>
2023-12-10 22:39:52 +0300

    Added new file formats to read (txt and dat)

--- gui_description.py
+++ gui_description.py
@@ -16,11 +16,12 @@ def make_dpi_aware():
 make_dpi_aware()
 
 # Style settings
-sg.theme('SystemDefaultForReal')   # Add a little color to windows
+sg.theme('GrayGrayGray')   # Add a little color to windows
 # also good themes: 'DarkAmber', 'DarkGreen2', 'DarkGrey13', 'DarkGrey8', 'DarkTeal6', 'Default', 'Default1', 'GrayGrayGray', 'SystemDefaultForReal'
 text_font = 'SegoeUI 10' # also checked 'Arial', 'Calibri', 'Isocpeur', 'Menlo',  number is a text size
 text_font_small = 'SegoeUI 9' # for not so valuable things
 checkbox_size = (15)
+listbox_size = (15,4)
 menu_def = [['&File', ['Something']], ['&Help', ['&About']]]  # Menu string format
 
 
@@ -29,22 +30,25 @@ def make_gui_window():
     layout = [
         [ sg.Menu(menu_def, key='-MENU LINE-') ],
 
-        [ sg.Text('Log file (.csv):', font=text_font),
+        [ sg.Text('Log file (csv/dat/txt):', font=text_font),
           sg.InputText(font=text_font_small, size=(50,10), expand_x=True, key='-FILEPATH-'),
-          sg.FileBrowse(file_types=(('*.csv', '*.csv*'),), font=text_font, auto_size_button=True) ],
+          sg.FileBrowse(file_types=[("*.csv", "*.csv*"), ("*.txt", "*.txt*"), ("*.dat", "*.dat*")], font=text_font, auto_size_button=True) ],  #, '*.dat', '*.dat*'
 
         [ sg.Button('Read', font=text_font, key='-READ-') ],
         # какой-то график непонятный пока
         # [ sg.Graph(canvas_size=(50,50), graph_bottom_left=(10,50), graph_top_right=(50,10), expand_x=True, expand_y=True, background_color='Black') ],
         [ sg.HorizontalSeparator(color='grey') ],
         # [ sg.Listbox(values=[1,2,3,4]) ],
+
+        [ sg.Text('Alice IDs in log:')],
+        [ sg.Listbox(values=[],font=text_font_small, size=listbox_size, expand_x=False, expand_y=True, select_mode='single', change_submits=False, key='-ALICE_ID_LIST-'),
+          sg.Button('Select Alice ID >>>', font=text_font, key='-ID_SELECT-'),
+          # sg.Text('>>>', font=text_font),
+          sg.InputText(default_text='', font=text_font_small, size=(15, 10), expand_x=False, disabled=True, use_readonly_for_disable=True, key='-SELECTED_ID-')],
+
         [ sg.Text('Parameters for plotting:') ],
-        [ sg.Checkbox('QBER', font=text_font, size=checkbox_size, key='-QBER-', default=True)],
-        [ sg.Checkbox('Piezo voltage', font=text_font, size=checkbox_size, key='-PIEZO-'), sg.Checkbox('Efficiency', font=text_font, size=checkbox_size, key='-EFFICIENCY-') ],
-        [ sg.Checkbox('Visibility', font=text_font, size=checkbox_size, key='-VISIBILITY-'), sg.Checkbox('Secret key length', font=text_font, size=checkbox_size, key='-SECRET KEY-') ],
-        [ sg.Text('List of parameters for plotting:') ],
-        [ sg.Listbox(values=[],font=text_font_small, size=(30,10), expand_x=False, expand_y=True, select_mode='multiple', change_submits=False, key='-PARAM_LIST-') ],
-        [ sg.Button('Select', font=text_font, key='-PARAM_SELECT-') ],
+        [ sg.Listbox(values=[],font=text_font_small, size=listbox_size, expand_x=False, expand_y=True, select_mode='multiple', change_submits=False, key='-PARAM_LIST-')],
+        [ sg.Button('Select parameters', font=text_font, key='-PARAM_SELECT-') ],
 
         [ sg.Text('Actions log:', expand_x=True)],
         [ sg.Multiline(size=(80,10), font='Courier 8', expand_x=True, expand_y=True, write_only=True, reroute_stdout=True,
--- main.py
+++ main.py
@@ -40,31 +40,63 @@ def read_csv(csv_filepath):
         # print('Input data type is ', type(data))
     return data
 
+def read_txt(txt_filepath):
+    data = pandas.DataFrame()  # initialization for exception errors when filepath is empty
+    if txt_filepath == '':
+        print('[LOG] [Err] File reading error: Filepath is empty or unknown')
+    else:
+        # *** Чтение txt файла:
+        data = pd.read_csv(txt_filepath, delimiter = "\s+" )  #delimiter = " "
+        print('[LOG] Input txt table:\n', data)
+        # print('Input data type is ', type(data))
+    return data
+
+
 def group_by_AliceID(data):    # *** Группировка по ID Алис ***
     # grouped_data = []
+    alice_id_list = []  #инициализация
+    if 'Alice ID' not in data.keys():
+        alice_id0 = '0x0'
+        data.loc[:,'Alice ID'] = alice_id0
+        print('There isn\'t Alice ID, so it named', alice_id0)
+
     grouped_data = data.groupby('Alice ID')
     alice_id_list = list(grouped_data.groups.keys())
     print('Alice ID list: \n', alice_id_list)
+    # Тут че-то надо дописать,  сделать сгруппированный массив при одной Адисе
     return grouped_data, alice_id_list
 
-def data_processing(data):
-    data_proc = data
-    # *** Группировка по ID Алис ***
-    if 'Alice ID' in data.keys():
-        data_grouped_id, alice_ids = group_by_AliceID(data)
-        for idx in alice_ids:
-            print('Alice ID: ', idx)
-            data_id = data_grouped_id.get_group(idx)
-            parameters_list = data_id.columns
-            print('List of parameters for Alice', str(idx), ': \n', parameters_list)
-    else:
-        alice_id0 = '0x0'
-        print('There isn\'t Alice ID, so it named', alice_id0)
-        data_id = data
-        parameters_list = data_id.columns
-        print('List of parameters for alone Alice', str(alice_id0), ': \n', parameters_list)
 
-    return data_proc, parameters_list
+def get_parameters_list(data_id_grouped, IDx):
+    print('Alice ID: ', IDx)
+    data_single_id = data_id_grouped.get_group(IDx)
+    parameters_list = data_single_id.columns
+    print('List of parameters for Alice', str(IDx), ': \n', parameters_list)
+    return data_single_id, parameters_list
+
+# def data_processing(data):
+    # Эта функция бесполезна, надо сделать функцию, которая вытаскивает параметры для конкретной Алисы, входные данные - ID Алисы, выходные - список параметров, которые отображать в интерфейсе
+    # data_proc = data
+    # *** Группировка по ID Алис ***
+    # if 'Alice ID' in data.keys():
+    #     data_grouped_id, alice_ids = group_by_AliceID(data)
+        # for idx in alice_ids:
+        #     print('Alice ID: ', idx)
+        #     data_id = data_grouped_id.get_group(idx)
+        #     parameters_list = data_id.columns
+        #     print('List of parameters for Alice', str(idx), ': \n', parameters_list)
+    # else:
+    #     alice_id0 = '0x0'
+    #     alice_ids = [alice_id0]
+        # print('There isn\'t Alice ID, so it named', alice_id0)
+        # data_id0 = data
+        # parameters_list = data_id0.columns
+        # print('List of parameters for alone Alice', str(alice_id0), ': \n', parameters_list)
+
+    # return data_proc, alice_ids, parameters_list
+
+# def plotting_for_single_alice(data, id_num)
+#     data_id = data.get_group(idx)
 
 
 
@@ -72,6 +104,7 @@ def data_processing(data):
 filepath = ''
 # input_data = []
 input_data = pandas.DataFrame()
+alice_id_selected = ''
 qkd_params = []
 qkd_params_selected = []
 
@@ -99,13 +132,33 @@ while True:  # The Event Loop
         #     print(key, ' = ', values[key])
 
         if event == '-READ-':
+            alice_ID_list = [] # очистка поля ID на всякий случай
+            qkd_params = [] # очистка поля параметров на всякий случай
             filepath = values['-FILEPATH-']
-            input_data = read_csv(filepath)
+            if filepath == '':
+                print('[LOG] [Err] File reading error: Filepath is empty or unknown')
+            elif 'csv' in filepath:
+                input_data = read_csv(filepath)
+            elif ('txt' in filepath) or ('dat' in filepath):
+                input_data = read_txt(filepath)
+
             if input_data.empty:
                 print('Input data is empty')
             else:
-                data_proc, qkd_params = data_processing(input_data)
-                main_window['-PARAM_LIST-'].update(values=qkd_params)
+                data_proc, alice_ID_list = group_by_AliceID(input_data)
+                # print(data_proc)
+            main_window['-ALICE_ID_LIST-'].update(values=alice_ID_list)
+            main_window['-PARAM_LIST-'].update(values=qkd_params)
+
+        elif event == '-ID_SELECT-':
+            alice_id_selected = values['-ALICE_ID_LIST-']
+            alice_id_selected = alice_id_selected[0]
+            print('Selected Alice ID ', alice_id_selected)
+            print('Type is ', type(alice_id_selected))
+            main_window['-SELECTED_ID-'].update(alice_id_selected)
+            data_proc_selected_alice = data_proc.get_group(alice_id_selected)
+            qkd_params = data_proc_selected_alice.columns
+            main_window['-PARAM_LIST-'].update(values=qkd_params)
 
         elif event == '-PARAM_SELECT-':
             qkd_params_selected = values['-PARAM_LIST-']
@@ -122,6 +175,9 @@ while True:  # The Event Loop
             else:
                 # то можно строить график
                 print('Trying to plot selected parameters..')
+                for idx in alice_id_selected:
+                    plotting_for_single_alice(data_proc, idx)
+
 
 
 
