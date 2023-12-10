c5a6322a7902ac637d681430737b824cbc965a3c
alinkazhoptic <borisova_alina_95@mail.ru>
2023-12-11 02:18:56 +0300

    Added clearing data and graph plotting, that works only for dat

--- gui_description.py
+++ gui_description.py
@@ -24,6 +24,8 @@ checkbox_size = (15)
 listbox_size = (15,4)
 menu_def = [['&File', ['Something']], ['&Help', ['&About']]]  # Menu string format
 
+table_data = []
+table_headings = []
 
 # Window description
 def make_gui_window():
@@ -31,34 +33,46 @@ def make_gui_window():
         [ sg.Menu(menu_def, key='-MENU LINE-') ],
 
         [ sg.Text('Log file (csv/dat/txt):', font=text_font),
-          sg.InputText(font=text_font_small, size=(50,10), expand_x=True, key='-FILEPATH-'),
+          sg.InputText(key='-FILEPATH-', font=text_font_small, size=(50,10), expand_x=True),
           sg.FileBrowse(file_types=[("*.csv", "*.csv*"), ("*.txt", "*.txt*"), ("*.dat", "*.dat*")], font=text_font, auto_size_button=True) ],  #, '*.dat', '*.dat*'
 
-        [ sg.Button('Read', font=text_font, key='-READ-') ],
+        [ sg.Button(key='-READ-', button_text='Read', font=text_font) ],
         # какой-то график непонятный пока
         # [ sg.Graph(canvas_size=(50,50), graph_bottom_left=(10,50), graph_top_right=(50,10), expand_x=True, expand_y=True, background_color='Black') ],
         [ sg.HorizontalSeparator(color='grey') ],
         # [ sg.Listbox(values=[1,2,3,4]) ],
 
         [ sg.Text('Alice IDs in log:')],
-        [ sg.Listbox(values=[],font=text_font_small, size=listbox_size, expand_x=False, expand_y=True, select_mode='single', change_submits=False, key='-ALICE_ID_LIST-'),
-          sg.Button('Select Alice ID >>>', font=text_font, key='-ID_SELECT-'),
+        [ sg.Listbox(key='-ALICE_ID_LIST-', values=[],font=text_font_small, size=listbox_size, expand_x=False, expand_y=True, select_mode='single', change_submits=False),
+          sg.Button(key='-ID_SELECT-', button_text='Select Alice ID >>>', font=text_font),
           # sg.Text('>>>', font=text_font),
-          sg.InputText(default_text='', font=text_font_small, size=(15, 10), expand_x=False, disabled=True, use_readonly_for_disable=True, key='-SELECTED_ID-')],
+          sg.InputText(key='-SELECTED_ID-', default_text='', font=text_font_small, size=(15, 10), expand_x=False, disabled=True, use_readonly_for_disable=True)],
 
         [ sg.Text('Parameters for plotting:') ],
-        [ sg.Listbox(values=[],font=text_font_small, size=listbox_size, expand_x=False, expand_y=True, select_mode='multiple', change_submits=False, key='-PARAM_LIST-')],
-        [ sg.Button('Select parameters', font=text_font, key='-PARAM_SELECT-') ],
+        [ sg.Listbox(key='-PARAM_LIST-', values=[],font=text_font_small, size=listbox_size, expand_x=False, expand_y=True, select_mode='multiple', change_submits=False),
+          sg.Button(key='-PARAM_SELECT-', button_text='>> Select parameters >>', font=text_font),
+          sg.Multiline(key='-PARAM_SELECTED_LIST-', default_text='', font=text_font_small, size=(15, 10), expand_x=False, disabled=True)],
+
+        # # Таблица с параметрами не работает
+        # [ sg.Button(key='-TO_TABLE-', button_text='Add data to table', font=text_font)],
+        # [ sg.Table(key='-TABLE-', values=table_data, headings=table_headings, max_col_width=25,
+        #           background_color=[0.9, 0.9, 0.9],
+        #           auto_size_columns=True,
+        #           display_row_numbers=True,
+        #           justification='right',
+        #           num_rows=10,
+        #           alternating_row_color=[0.7, 0.7, 0.7],
+        #           row_height=25)],
 
         [ sg.Text('Actions log:', expand_x=True)],
         [ sg.Multiline(size=(80,10), font='Courier 8', expand_x=True, expand_y=True, write_only=True, reroute_stdout=True,
                        reroute_stderr=True, reroute_cprint=True, echo_stdout_stderr=True, autoscroll=True, auto_refresh=True)],
 
-        [ sg.Button('Plot', font=text_font, key='-PLOT-') ],
-        [ sg.Button('Clear', font=text_font, key='-CLEAR-') ],
+        [ sg.Button(key='-PLOT-', button_text='Plot', font=text_font) ],
+        [ sg.Button(key='-CLEAR-', button_text='Clear', font=text_font) ],
 
-        [ sg.Submit(font=text_font, key='-SUBMIT-'),
-          sg.Cancel(font=text_font, key='-CANCEL-') ]
+        [ sg.Submit(key='-SUBMIT-', font=text_font),
+          sg.Cancel(key='-CANCEL-', font=text_font) ]
     ]
     layout[-1].append(sg.Sizegrip())
 
--- main.py
+++ main.py
@@ -40,13 +40,14 @@ def read_csv(csv_filepath):
         # print('Input data type is ', type(data))
     return data
 
+
 def read_txt(txt_filepath):
     data = pandas.DataFrame()  # initialization for exception errors when filepath is empty
     if txt_filepath == '':
         print('[LOG] [Err] File reading error: Filepath is empty or unknown')
     else:
         # *** Чтение txt файла:
-        data = pd.read_csv(txt_filepath, delimiter = "\s+" )  #delimiter = " "
+        data = pd.read_csv(txt_filepath, delimiter = "\s+" )
         print('[LOG] Input txt table:\n', data)
         # print('Input data type is ', type(data))
     return data
@@ -74,29 +75,69 @@ def get_parameters_list(data_id_grouped, IDx):
     print('List of parameters for Alice', str(IDx), ': \n', parameters_list)
     return data_single_id, parameters_list
 
-# def data_processing(data):
-    # Эта функция бесполезна, надо сделать функцию, которая вытаскивает параметры для конкретной Алисы, входные данные - ID Алисы, выходные - список параметров, которые отображать в интерфейсе
-    # data_proc = data
-    # *** Группировка по ID Алис ***
-    # if 'Alice ID' in data.keys():
-    #     data_grouped_id, alice_ids = group_by_AliceID(data)
-        # for idx in alice_ids:
-        #     print('Alice ID: ', idx)
-        #     data_id = data_grouped_id.get_group(idx)
-        #     parameters_list = data_id.columns
-        #     print('List of parameters for Alice', str(idx), ': \n', parameters_list)
-    # else:
-    #     alice_id0 = '0x0'
-    #     alice_ids = [alice_id0]
-        # print('There isn\'t Alice ID, so it named', alice_id0)
-        # data_id0 = data
-        # parameters_list = data_id0.columns
-        # print('List of parameters for alone Alice', str(alice_id0), ': \n', parameters_list)
 
-    # return data_proc, alice_ids, parameters_list
+def plotting_for_single_alice(data, params):
+    num_of_params = len(params)
+    print('num_of_params = ', num_of_params)
 
-# def plotting_for_single_alice(data, id_num)
-#     data_id = data.get_group(idx)
+    # Colors definitions
+    c_red = [0.8, 0, 0]
+    c_blue = [0, 0.3, 0.5]
+    c_green = [0, 0.5, 0]
+
+    # Line parameters
+    line_width = 1
+    grid_width = 0.5
+    # Marker parameters
+    marker_size = 10
+    marker_size_on_line = 4
+
+    # Labels
+    x_name = 'Time points'
+
+    text_for_graphs = ''
+
+    fig1 = plt.figure(figsize=[8, 6])
+
+    plt.subplots_adjust(wspace=0.4, hspace=0.4)
+    plt.rcParams['font.size'] = '8'
+    plt.rcParams['font.family'] = 'Arial'
+
+    fig1.suptitle('QKD output parameters', fontsize=10)
+    num_of_axs = num_of_params + 1
+    print('num_of_axs = ', num_of_axs)
+
+    for i in range(num_of_params):
+
+        # Расчет средних значенийи СКО:
+        p = params[i]
+        data_p = data[p].to_numpy(na_value=0)
+        print('param = ', p)
+        print( '\ndata(p) = \n', data_p )
+        mean_p = np.mean( data_p )
+        SKO_p = np.std( data_p )
+        text_for_graphs = text_for_graphs + p + ': \nmean = ' + str("%.2f" % mean_p) + '\nstandard deviation = ' + str("%.2f" % SKO_p) + '\n\n'
+
+        print('i = ', i)
+        ax = plt.subplot(num_of_axs, 1, i+1)
+        ax.plot( data_p, color=c_blue, linestyle='--', linewidth=line_width)
+        ax.set(ylabel=params[i])
+        ax.grid(True, linestyle=':', linewidth=grid_width)
+        ax.legend(['data'], loc='upper right')
+
+    ax_Text = plt.subplot(num_of_axs, 1, num_of_axs)
+    ax_Text.axis('off')
+    ax_Text.text(0.1, 0.2, text_for_graphs, fontsize=10)
+
+
+    return fig1
+
+
+
+
+
+
+
 
 
 
@@ -156,16 +197,39 @@ while True:  # The Event Loop
             print('Selected Alice ID ', alice_id_selected)
             print('Type is ', type(alice_id_selected))
             main_window['-SELECTED_ID-'].update(alice_id_selected)
-            data_proc_selected_alice = data_proc.get_group(alice_id_selected)
-            qkd_params = data_proc_selected_alice.columns
+
+            data_proc_selected_alice = data_proc.get_group(alice_id_selected)  # Parameters for only one Alice if there are many ones
+            qkd_params = data_proc_selected_alice.columns  # ALL the parameters of QKD in log
             main_window['-PARAM_LIST-'].update(values=qkd_params)
 
         elif event == '-PARAM_SELECT-':
             qkd_params_selected = values['-PARAM_LIST-']
+            main_window['-PARAM_SELECTED_LIST-'].update(qkd_params_selected)
             print('Selected parameters for graph: \n', qkd_params_selected )
+
+        # # Таблица с параметрами не работает
+        # elif event == '-TO_TABLE-':
+        #     print('Writing selected data to table')
+        #     for i in range(len(qkd_params_selected)):
+        #         main_window['-TABLE-'].update(values=data_proc_selected_alice[qkd_params_selected[i]])
+        #     print('Table was filled with data')
+
 
         elif event == '-CLEAR-':
             input_data = pandas.DataFrame()
+
+            alice_ID_list = []
+            main_window['-ALICE_ID_LIST-'].update(values=alice_ID_list)
+
+            alice_id_selected = ''
+            main_window['-SELECTED_ID-'].update(alice_id_selected)
+
+            qkd_params = []
+            main_window['-PARAM_LIST-'].update(qkd_params)
+
+            qkd_params_selected = []
+            main_window['-PARAM_SELECTED_LIST-'].update(qkd_params_selected)
+
             print('Type of cleared input data is ', type(input_data))
             print('!!! Data cleared !!!')
 
@@ -174,9 +238,12 @@ while True:  # The Event Loop
                 print('No selected parameters')
             else:
                 # то можно строить график
+                print('Selecting and processing to numpy ')
+
                 print('Trying to plot selected parameters..')
-                for idx in alice_id_selected:
-                    plotting_for_single_alice(data_proc, idx)
+                fig_graph = plotting_for_single_alice(data_proc_selected_alice, qkd_params_selected)
+                plt.show()  # Отображаем графики
+
 
 
 
